1c2e573cb1c0cc4b5c188244928ac057b5446a6b
Diogo Silva <diogosilvaoficial@gmail.com>
2017-07-25 19:13:29 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+
+    <title>Diogo Silva - Site Oficial</title>
+
+    <!-- Meta -->
+    <meta name="author" content="DS WebSolution">
+    <meta name="generator" content="DS WebSolution - Criação e Desenvolvimento de Websites em geral.">
+
+    <!-- Css -->
+
+    <link rel="stylesheet" href="itens/css/estilos.css" />
+    <link rel="stylesheet" href="itens/css/font-awesome/css/font-awesome.css" />
+    <link rel="stylesheet" href="itens/css/font-awesome/css/font-awesome.min.css" />
+
+
+    <!-- Scripts & Bibliotecas -->
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+    <script src="itens/js/scroll_ancora.js"></script>
+
+    <!-- Schema -->
+    <script type="application/ld+json">
+        {
+        "@Context": "http://schema.org",
+        "@Type": "MusicGroup",
+        "name": "Diogo Silva",
+        "url": "http://www.diogosilvaoficial.com.br",
+        "sameAs": [
+        "https://www.facebook.com/cantordiogosilva/",
+        "https://www.youtube.com/user/diogosilvaoficial/",
+        "https://www.instagram.com/cantordiogosilva/",
+        "https://twitter.com/diogosilvamusic"
+        ],
+        "logo": "http://www.diogosilvaoficial.com.br/itens/imagens/logo.png",
+        "genre": "Sertanejo"
+        }
+    </script>
+
+
+
+
+</head>
+<body>
+
+<!-- TOPO DO SITE -->
+<header>
+    <section class="home" id="home">
+        <strong>Home</strong>
+    </section>
+</header>
+
+<!-- NAVEGAÇÃO -->
+<nav class="menu">
+    <div class="logo-menu">
+        <img src="itens/imagens/logo-menu.png" />
+    </div>
+
+    <ul>
+        <li><a href="#home">HOME</a></li>
+        <li><a href="#agenda">AGENDA</a></li>
+        <li><a href="#release">RELEASE</a></li>
+        <li><a href="#redessociais">REDES SOCIAIS</a></li>
+        <li><a href="#musicas">MÚSICAS</a></li>
+        <li><a href="#contato">CONTATO</a></li>
+    </ul>
+
+</nav>
+
+<nav class="social-icones">
+    <ul>
+        <li><i class="fa fa-facebook" aria-hidden="true"></i></li>
+        <li><i class="fa fa-instagram" aria-hidden="true"></i></li>
+        <li><i class="fa fa-youtube" aria-hidden="true"></i></li>
+        <li><i class="fa fa-twitter" aria-hidden="true"></i></li>
+        <li><i class="fa fa-spotify" aria-hidden="true"></i></li>
+
+    </ul>
+</nav>
+
+<!-- CONTEÚDO -->
+<article>
+
+    <!-- HOME -->
+    <section class="home" id="home">
+        <strong>Home</strong>
+    </section>
+
+    <!-- AGENDA -->
+    <section class="agenda" id="agenda">
+        <strong>Agenda</strong>
+    </section>
+
+    <!-- RELEASE -->
+    <section class="release">
+        <a id="release"><strong>Release</strong></a>
+    </section>
+
+    <!-- REDES SOCIAIS -->
+    <section class="redessociais">
+        <a id="redessociais"><strong>Redes Sociais</strong></a>
+    </section>
+
+    <!-- MÚSICAS -->
+    <section class="musicas">
+        <a id="musicas"><strong>Músicas</strong></a>
+    </section>
+
+    <!-- CONTATO -->
+    <section class="contato">
+        <a id="contato"><strong>Contato</strong></a>
+    </section>
+</article>
+<!-- RODAPÉ -->
+<footer class="footer">
+
+</footer>
+
+
+</body>
+</html>
